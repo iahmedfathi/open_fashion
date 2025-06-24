092d45c7df253a5a52d439c061539aff641dd84c
ahmed fathi <ahmedfathi.developer@gmail.com>
2025-06-24 19:07:39 +0300

    change color atrubite

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -18,16 +18,16 @@ class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
       padding: const EdgeInsets.all(8),
       child: AppBar(
         actions: [
-          SvgPicture.asset("assets/svgs/Search.svg",color: isBlack ? Colors.white : AppColors.primary),
+          SvgPicture.asset("assets/svgs/Search.svg",colorFilter:ColorFilter.mode(isBlack ? Colors.white : AppColors.primary,  BlendMode.srcIn) ),
           Gap(20),
-          SvgPicture.asset("assets/svgs/shopping bag.svg",color: isBlack ? Colors.white : AppColors.primary),
+          SvgPicture.asset("assets/svgs/shopping bag.svg",colorFilter:ColorFilter.mode(isBlack ? Colors.white : AppColors.primary,  BlendMode.srcIn)),
         ],
         leadingWidth: 25,
         centerTitle: true,
         scrolledUnderElevation: 0.0,
         backgroundColor: isBlack ? AppColors.primary : Colors.white,
-        leading: SvgPicture.asset("assets/svgs/Menu.svg",color: isBlack ? Colors.white : AppColors.primary),
-        title: SvgPicture.asset("assets/logo/logo-bg.svg",color: isBlack ? Colors.white : AppColors.primary),
+        leading: SvgPicture.asset("assets/svgs/Menu.svg",colorFilter:ColorFilter.mode(isBlack ? Colors.white : AppColors.primary,  BlendMode.srcIn)),
+        title: SvgPicture.asset("assets/logo/logo-bg.svg",colorFilter:ColorFilter.mode(isBlack ? Colors.white : AppColors.primary,  BlendMode.srcIn)),
       ),
     );
   }
